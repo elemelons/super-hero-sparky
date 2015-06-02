a7564bf581c7be2ba70fc5a5a1ae00b869162956
lentekal <kalvanlent@gmail.com>
2015-06-02 14:13:52 +0200

    Tin cans start
    gestart aan de tincans en een ball class

---
+++ Source/Dogware/Dogware/Dogware/Objects/tin cans/ball.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimGame;
+
+namespace Dogware.Objects.tin_cans
+{
+    class ball : GameObject
+    {
+        private bool thrown = false;
+
+        public ball(Vector2 position) : base("Ball", false, position, "ball.png")
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+
+        }
+
+        public override void OnCollision(GameObject other)//ik kom tegen "other" aan
+        {
+            if(other.Name == "Blikje")//is de naam van het voorwerp waar ik tegenaankom "Blikje"?
+            {
+
+            }
+        }
+    }
+}
